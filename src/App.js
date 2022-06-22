41281af62485a6ac9cdf657adc9de83167a80a67
kabir <makkobir79@gmail.com>
2022-06-22 23:04:10 +0600

    login and signup component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./pages/shared/header/Header";
 import Footer from "./pages/shared/footer/Footer";
+import Login from "./pages/login/login/Login";
+import Signup from "./pages/login/signup/Signup";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Login />
+      <Signup />
+
       <Footer />
     </div>
   );
